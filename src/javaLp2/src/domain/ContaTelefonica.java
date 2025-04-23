22210646490bf606464367c65bd334585eb561f1
Samuel Cesar <99971558+sCesar-dev@users.noreply.github.com>
2025-04-23 09:03:23 -0300

    Create ContaTelefonica.java

---
+++ src/javaLp2/src/domain/ContaTelefonica.java
@@ -0,0 +1,6 @@
+package domain;
+
+public class ContaTelefonica{
+  private String nrTelOrigem, nmLocOrigem, nrTelDestino, nmLocDestino;
+  private float valTotLigacao;
+  public void criaLigacao(  
